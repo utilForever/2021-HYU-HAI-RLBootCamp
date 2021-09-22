fc29365d7c55cf682819e18f5b14b9ed718084b1
Chris Ohk <utilforever@gmail.com>
2021-09-22 17:29:02 +0900

    Upload code '02 - DQN N-Step'

---
+++ 2 - Code/210924 - DQN Extensions #1/02 - DQN N-Step.py
@@ -0,0 +1,63 @@
+import argparse
+import gym
+import ptan
+import random
+
+import torch
+import torch.optim as optim
+
+from ignite.engine import Engine
+
+from lib import dqn_model, epsilon_tracker, hyper_params, utils
+
+DEFAULT_N_STEPS = 4
+
+if __name__ == "__main__":
+    random.seed(hyper_params.SEED)
+    torch.manual_seed(hyper_params.SEED)
+
+    params = hyper_params.HYPER_PARAMS['pong']
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cuda", default=False, action="store_true", help="Enable CUDA")
+    parser.add_argument("-n", type=int, default=DEFAULT_N_STEPS, help="Steps to do on Bellman unroll")
+    args = parser.parse_args()
+
+    device = torch.device("cuda" if args.cuda else "cpu")
+
+    env = gym.make(params.env_name)
+    env = ptan.common.wrappers.wrap_dqn(env)
+    env.seed(hyper_params.SEED)
+
+    net = dqn_model.DQN(env.observation_space.shape, env.action_space.n).to(device)
+    target_net = ptan.agent.TargetNet(net)
+    selector = ptan.actions.EpsilonGreedyActionSelector(epsilon=params.epsilon_start)
+    epsilon_tracker = epsilon_tracker.EpsilonTracker(selector, params)
+    agent = ptan.agent.DQNAgent(net, selector, device=device)
+
+    exp_source = ptan.experience.ExperienceSourceFirstLast(env, agent, gamma=params.gamma, steps_count=args.n)
+    buffer = ptan.experience.ExperienceReplayBuffer(exp_source, buffer_size=params.replay_size)
+    optimizer = optim.Adam(net.parameters(), lr=params.learning_rate)
+
+
+    def process_batch(engine_for_batch, batch):
+        optimizer.zero_grad()
+
+        loss_v = utils.calc_loss_dqn(batch, net, target_net.target_model, gamma=params.gamma ** args.n, device=device)
+        loss_v.backward()
+
+        optimizer.step()
+        epsilon_tracker.frame(engine_for_batch.state.iteration)
+
+        if engine_for_batch.state.iteration % params.target_net_sync == 0:
+            target_net.sync()
+
+        return {
+            "loss": loss_v.item(),
+            "epsilon": selector.epsilon,
+        }
+
+
+    engine = Engine(process_batch)
+    utils.setup_ignite(engine, params, exp_source, f"02_N_Step={args.n}")
+    engine.run(utils.batch_generator(buffer, params.replay_initial, params.batch_size))
