5f8e1808e2d9a1c4e32cc35c74c9aab958273253
Chris Ohk <utilforever@gmail.com>
2021-09-26 22:45:33 +0900

    Add function 'distr_projection()'

--- 2 - Code/210928 - DQN Extensions #2/lib/dqn_extra.py
+++ 2 - Code/210928 - DQN Extensions #2/lib/dqn_extra.py
@@ -11,6 +11,11 @@ import numpy as np
 BETA_START = 0.4
 BETA_FRAMES = 100000
 
+V_MAX = 10
+V_MIN = -10
+N_ATOMS = 51
+DELTA_Z = (V_MAX - V_MIN) / (N_ATOMS - 1)
+
 
 class NoisyLinear(nn.Linear):
     def __init__(self, in_features, out_features, sigma_init=0.017, bias=True):
@@ -195,3 +200,47 @@ class DuelingDQN(nn.Module):
 
     def _forward_unimplemented(self, *input_forward: Any) -> None:
         pass
+
+
+def distr_projection(next_distr, rewards, dones, gamma):
+    batch_size = len(rewards)
+    proj_distr = np.zeros((batch_size, N_ATOMS), dtype=np.float32)
+    delta_z = (V_MAX - V_MIN) / (N_ATOMS - 1)
+
+    for atom in range(N_ATOMS):
+        v = rewards + (V_MIN + atom * delta_z) * gamma
+        tz_j = np.minimum(V_MAX, np.maximum(V_MIN, v))
+        b_j = (tz_j - V_MIN) / delta_z
+        l = np.floor(b_j).astype(np.int64)
+        u = np.ceil(b_j).astype(np.int64)
+
+        eq_mask = u == l
+        proj_distr[eq_mask, l[eq_mask]] += next_distr[eq_mask, atom]
+
+        ne_mask = u != l
+        proj_distr[ne_mask, l[ne_mask]] += next_distr[ne_mask, atom] * (u - b_j)[ne_mask]
+        proj_distr[ne_mask, u[ne_mask]] += next_distr[ne_mask, atom] * (b_j - l)[ne_mask]
+
+    if dones.any():
+        proj_distr[dones] = 0.0
+        tz_j = np.minimum(V_MAX, np.maximum(V_MIN, rewards[dones]))
+        b_j = (tz_j - V_MIN) / delta_z
+        l = np.floor(b_j).astype(np.int64)
+        u = np.ceil(b_j).astype(np.int64)
+
+        eq_mask = u == l
+        eq_dones = dones.copy()
+        eq_dones[dones] = eq_mask
+
+        if eq_dones.any():
+            proj_distr[eq_dones, l[eq_mask]] = 1.0
+
+        ne_mask = u != l
+        ne_dones = dones.copy()
+        ne_dones[dones] = ne_mask
+
+        if ne_dones.any():
+            proj_distr[ne_dones, l[ne_mask]] = (u - b_j)[ne_mask]
+            proj_distr[ne_dones, u[ne_mask]] = (b_j - l)[ne_mask]
+
+    return proj_distr
