13b5fbcb80b3f18ba698a5702418edd1a2a1d216
Chris Ohk <utilforever@gmail.com>
2021-09-27 00:49:03 +0900

    Add class 'RainbowDQN'

--- 2 - Code/210928 - DQN Extensions #2/lib/dqn_extra.py
+++ 2 - Code/210928 - DQN Extensions #2/lib/dqn_extra.py
@@ -293,3 +293,42 @@ class CategoricalDQN(nn.Module):
 
     def apply_softmax(self, t):
         return self.softmax(t.view(-1, N_ATOMS)).view(t.size())
+
+
+class RainbowDQN(nn.Module):
+    def __init__(self, input_shape, num_actions):
+        super(RainbowDQN, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        conv_out_size = self._get_conv_out(input_shape)
+        self.fc_adv = nn.Sequential(
+            NoisyLinear(conv_out_size, 256),
+            nn.ReLU(),
+            NoisyLinear(256, num_actions)
+        )
+        self.fc_val = nn.Sequential(
+            nn.Linear(conv_out_size, 256),
+            nn.ReLU(),
+            nn.Linear(256, 1)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        adv, val = self.adv_val(x)
+        return val + (adv - adv.mean(dim=1, keepdim=True))
+
+    def adv_val(self, x):
+        fx = x.float() / 256
+        conv_out = self.conv(fx).view(fx.size()[0], -1)
+        return self.fc_adv(conv_out), self.fc_val(conv_out)
