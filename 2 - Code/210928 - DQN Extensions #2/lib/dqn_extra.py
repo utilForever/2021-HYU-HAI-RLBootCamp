8dff3f492e727f5fdfd270a85afca6a01356182c
Chris Ohk <utilforever@gmail.com>
2021-09-26 21:29:23 +0900

    Add class 'DuelingDQN'

--- 2 - Code/210928 - DQN Extensions #2/lib/dqn_extra.py
+++ 2 - Code/210928 - DQN Extensions #2/lib/dqn_extra.py
@@ -152,3 +152,46 @@ class PrioritizedReplayBuffer:
     def update_priorities(self, batch_indices, batch_priorities):
         for idx, priority in zip(batch_indices, batch_priorities):
             self.priorities[idx] = priority
+
+
+class DuelingDQN(nn.Module):
+    def __init__(self, input_shape, n_actions):
+        super(DuelingDQN, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32,
+                      kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        conv_out_size = self._get_conv_out(input_shape)
+        self.fc_adv = nn.Sequential(
+            nn.Linear(conv_out_size, 256),
+            nn.ReLU(),
+            nn.Linear(256, n_actions)
+        )
+        self.fc_val = nn.Sequential(
+            nn.Linear(conv_out_size, 256),
+            nn.ReLU(),
+            nn.Linear(256, 1)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        adv, val = self.adv_val(x)
+        return val + (adv - adv.mean(dim=1, keepdim=True))
+
+    def adv_val(self, x):
+        fx = x.float() / 256
+        conv_out = self.conv(fx).view(fx.size()[0], -1)
+        return self.fc_adv(conv_out), self.fc_val(conv_out)
+
+    def _forward_unimplemented(self, *input_forward: Any) -> None:
+        pass
