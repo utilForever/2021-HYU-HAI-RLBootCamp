972e1105a9f550e62783abc6c375665d5ae1af4b
Chris Ohk <utilforever@gmail.com>
2021-10-07 01:23:55 +0900

    Update code '01 - A2C'

---
+++ 2 - Code/211008 - Actor-Critic Extensions/01 - A2C.py
@@ -0,0 +1,171 @@
+import argparse
+import gym
+import ptan
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.utils as nn_utils
+import torch.nn.functional as F
+import torch.optim as optim
+
+from typing import Any
+from tensorboardX import SummaryWriter
+
+from lib import utils
+
+GAMMA = 0.99
+LEARNING_RATE = 0.001
+ENTROPY_BETA = 0.01
+BATCH_SIZE = 128
+NUM_ENVS = 50
+
+REWARD_STEPS = 4
+CLIP_GRAD = 0.1
+
+
+class A2C(nn.Module):
+    def __init__(self, input_shape, num_actions):
+        super(A2C, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        conv_out_size = self._get_conv_out(input_shape)
+        self.policy = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, num_actions)
+        )
+
+        self.value = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, 1)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        fx = x.float() / 256
+        conv_out = self.conv(fx).view(fx.size()[0], -1)
+        return self.policy(conv_out), self.value(conv_out)
+
+    def _forward_unimplemented(self, *input_forward: Any) -> None:
+        pass
+
+
+def unpack_batch(_batch, _net, _device='cpu'):
+    states = []
+    actions = []
+    rewards = []
+    not_done_idx = []
+    last_states = []
+
+    for idx, _exp in enumerate(_batch):
+        states.append(np.array(_exp.state, copy=False))
+        actions.append(int(_exp.action))
+        rewards.append(_exp.reward)
+
+        if _exp.last_state is not None:
+            not_done_idx.append(idx)
+            last_states.append(np.array(_exp.last_state, copy=False))
+
+    _states_v = torch.FloatTensor(np.array(states, copy=False)).to(_device)
+    _actions_t = torch.LongTensor(actions).to(_device)
+
+    rewards_np = np.array(rewards, dtype=np.float32)
+
+    if not_done_idx:
+        last_states_v = torch.FloatTensor(np.array(last_states, copy=False)).to(_device)
+        last_vals_v = _net(last_states_v)[1]
+        last_vals_np = last_vals_v.data.cpu().numpy()[:, 0]
+        last_vals_np *= GAMMA ** REWARD_STEPS
+        rewards_np[not_done_idx] += last_vals_np
+
+    ref_vals_v = torch.FloatTensor(rewards_np).to(_device)
+
+    return _states_v, _actions_t, ref_vals_v
+
+
+def make_env():
+    return ptan.common.wrappers.wrap_dqn(gym.make("PongNoFrameskip-v4"))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cuda", default=False, action="store_true", help="Enable cuda")
+    parser.add_argument("-n", "--name", required=True, help="Name of the run")
+    args = parser.parse_args()
+
+    device = torch.device("cuda" if args.cuda else "cpu")
+
+    envs = [make_env() for _ in range(NUM_ENVS)]
+    writer = SummaryWriter(comment="-pong-a2c_" + args.name)
+
+    net = A2C(envs[0].observation_space.shape, envs[0].action_space.n).to(device)
+    agent = ptan.agent.PolicyAgent(lambda x: net(x)[0], apply_softmax=True, device=device)
+    exp_source = ptan.experience.ExperienceSourceFirstLast(envs, agent, gamma=GAMMA, steps_count=REWARD_STEPS)
+
+    optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE, eps=1e-3)
+
+    batch = []
+
+    with utils.RewardTracker(writer, stop_reward=18) as tracker:
+        with ptan.common.utils.TBMeanTracker(writer, batch_size=10) as tb_tracker:
+            for step_idx, exp in enumerate(exp_source):
+                batch.append(exp)
+
+                new_rewards = exp_source.pop_total_rewards()
+                if new_rewards:
+                    if tracker.reward(new_rewards[0], step_idx):
+                        break
+
+                if len(batch) < BATCH_SIZE:
+                    continue
+
+                states_v, actions_t, vals_ref_v = unpack_batch(batch, net, _device=device)
+                batch.clear()
+
+                optimizer.zero_grad()
+                logits_v, value_v = net(states_v)
+                loss_value_v = F.mse_loss(value_v.squeeze(-1), vals_ref_v)
+
+                log_prob_v = F.log_softmax(logits_v, dim=1)
+                adv_v = vals_ref_v - value_v.detach()
+                log_prob_actions_v = adv_v * log_prob_v[range(BATCH_SIZE), actions_t]
+                loss_policy_v = -log_prob_actions_v.mean()
+
+                prob_v = F.softmax(logits_v, dim=1)
+                entropy_loss_v = ENTROPY_BETA * (prob_v * log_prob_v).sum(dim=1).mean()
+
+                loss_policy_v.backward(retain_graph=True)
+                grads = np.concatenate([p.grad.data.cpu().numpy().flatten()
+                                        for p in net.parameters()
+                                        if p.grad is not None])
+
+                loss_v = entropy_loss_v + loss_value_v
+                loss_v.backward()
+                nn_utils.clip_grad_norm_(net.parameters(), CLIP_GRAD)
+                optimizer.step()
+
+                loss_v += loss_policy_v
+
+                tb_tracker.track("advantage", adv_v, step_idx)
+                tb_tracker.track("values", value_v, step_idx)
+                tb_tracker.track("batch_rewards", vals_ref_v, step_idx)
+                tb_tracker.track("loss_entropy", entropy_loss_v, step_idx)
+                tb_tracker.track("loss_policy", loss_policy_v, step_idx)
+                tb_tracker.track("loss_value", loss_value_v, step_idx)
+                tb_tracker.track("loss_total", loss_v, step_idx)
+                tb_tracker.track("grad_l2", np.sqrt(np.mean(np.square(grads))), step_idx)
+                tb_tracker.track("grad_max", np.max(np.abs(grads)), step_idx)
+                tb_tracker.track("grad_var", np.var(grads), step_idx)
