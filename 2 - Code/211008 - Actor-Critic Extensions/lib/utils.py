cc7e4acea364f4b295ee4e3ce7fe9d19adb21313
Chris Ohk <utilforever@gmail.com>
2021-10-07 01:38:04 +0900

    Add class 'A2C'

--- 2 - Code/211008 - Actor-Critic Extensions/lib/utils.py
+++ 2 - Code/211008 - Actor-Critic Extensions/lib/utils.py
@@ -2,6 +2,48 @@ import sys
 import time
 import numpy as np
 
+import torch
+import torch.nn as nn
+
+
+class A2C(nn.Module):
+    def __init__(self, input_shape, num_actions):
+        super(A2C, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        conv_out_size = self._get_conv_out(input_shape)
+        self.policy = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, num_actions)
+        )
+
+        self.value = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, 1)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        fx = x.float() / 256
+        conv_out = self.conv(fx).view(fx.size()[0], -1)
+        return self.policy(conv_out), self.value(conv_out)
+
+    def _forward_unimplemented(self, *input_forward: Any) -> None:
+        pass
+
 
 class RewardTracker:
     def __init__(self, writer, stop_reward):
